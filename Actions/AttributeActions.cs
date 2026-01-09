61dca55f5a4f979ca7f4387e96ed549f417f4abc
cuongdeverloper <136320623+cuongdeverloper@users.noreply.github.com>
2026-01-10 02:01:47 +0700

    Dockpanel

---
+++ Actions/AttributeActions.cs
@@ -0,0 +1,68 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Colors; 
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Exercise.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Exercise.Actions
+{
+    public static class AttributeActions
+    {
+        public static ObservableCollection<AttributeItem> GetAttributes(ObjectId blockId)
+        {
+            var list = new ObservableCollection<AttributeItem>();
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+
+            using (Transaction tr = doc.Database.TransactionManager.StartTransaction())
+            {
+                BlockReference br = tr.GetObject(blockId, OpenMode.ForRead) as BlockReference;
+                if (br != null)
+                {
+                    foreach (ObjectId attId in br.AttributeCollection)
+                    {
+                        AttributeReference attRef = tr.GetObject(attId, OpenMode.ForRead) as AttributeReference;
+                        list.Add(new AttributeItem
+                        {
+                            Tag = attRef.Tag,
+                            Value = attRef.TextString
+                        });
+                    }
+                }
+                tr.Commit();
+            }
+            return list;
+        }
+
+        public static void UpdateAttributes(ObjectId blockId, IEnumerable<AttributeItem> items)
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+
+            using (DocumentLock loc = doc.LockDocument())
+            using (Transaction tr = doc.Database.TransactionManager.StartTransaction())
+            {
+                BlockReference br = tr.GetObject(blockId, OpenMode.ForRead) as BlockReference;
+                if (br != null)
+                {
+                    foreach (ObjectId attId in br.AttributeCollection)
+                    {
+                        AttributeReference attRef = tr.GetObject(attId, OpenMode.ForWrite) as AttributeReference;
+
+                        foreach (var item in items)
+                        {
+                            if (item.Tag == attRef.Tag)
+                            {
+                                attRef.TextString = item.Value;
+                                break;
+                            }
+                        }
+                    }
+                }
+                tr.Commit();
+                doc.Editor.Regen(); 
+            }
+        }
+    }
+}
--- Commands/AutoCADCommands.cs
+++ Commands/AutoCADCommands.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using Autodesk.AutoCAD.Runtime;
+﻿using Autodesk.AutoCAD.Runtime;
 using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Windows;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
 using Exercise.Views;
 using Exercise.ViewModels;
+using System.Windows.Forms.Integration;
 
 [assembly: CommandClass(typeof(Exercise.Commands.AppCommands))]
 
@@ -10,61 +13,70 @@ namespace Exercise.Commands
 {
     public class AppCommands : IExtensionApplication
     {
-        private static Window1 _window;
-
-        #region IExtensionApplication Members
+        private static PaletteSet _ps = null;
+        private static AttributeView _view = null;
+        private static AttributeViewModel _viewModel = null;
 
         public void Initialize()
         {
-            var ed = Application.DocumentManager.MdiActiveDocument.Editor;
-            ed.WriteMessage("\n[Exercise] Plugin đã được khởi tạo thành công!");
-        }
-
-        public void Terminate()
-        {
-            if (_window != null)
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc != null)
             {
-                _window.Close();
+                doc.Editor.WriteMessage("\n[Exercise] Plugin Edit Block Loaded!");
+                doc.Editor.SelectionAdded += OnSelectionAdded;
             }
         }
 
-        #endregion
+        public void Terminate() { }
 
-        #region Commands
-
-        [CommandMethod("HELLOWORLD")]
-        public void HelloWorld()
+        private void OnSelectionAdded(object sender, SelectionAddedEventArgs e)
         {
-            Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("\nHello, AutoCAD!");
-        }
+            if (_ps == null || !_ps.Visible) return;
 
-        [CommandMethod("VE_MLEADER")]
-        public void ShowMLeaderSetup()
-        {
-            try
+            ObjectId[] ids = e.AddedObjects.GetObjectIds();
+            if (ids.Length == 0) return;
+
+            ObjectId id = ids[0];
+
+            using (Transaction tr = Application.DocumentManager.MdiActiveDocument.TransactionManager.StartTransaction())
             {
-                if (_window != null && _window.IsVisible)
+                var obj = tr.GetObject(id, OpenMode.ForRead);
+                if (obj is BlockReference)
                 {
-                    _window.Activate();
-                    return;
+                    _viewModel.LoadData(id);
                 }
-
-                _window = new Window1();
-
-                var viewModel = new TextSetupViewModel();
-                _window.DataContext = viewModel;
-
-                Application.ShowModelessWindow(_window);
-
-                _window.Closed += (s, e) => { _window = null; };
-            }
-            catch (System.Exception ex)
-            {
-                var ed = Application.DocumentManager.MdiActiveDocument.Editor;
-                ed.WriteMessage("\nLỗi khi mở giao diện: " + ex.Message);
+                tr.Commit();
             }
         }
 
-        #endregion
+        [CommandMethod("SHOW_PANEL")]
+        public void ShowPanel()
+        {
+            if (_ps == null)
+            {
+                _ps = new PaletteSet("Quản Lý Block");
+                _ps.Size = new System.Drawing.Size(300, 600);
+                _ps.Dock = DockSides.Left;
+
+                // --- ĐOẠN SỬA LỖI TẠI ĐÂY ---
+
+                // 1. Khởi tạo WPF View và ViewModel bình thường
+                _view = new AttributeView();
+                _viewModel = new AttributeViewModel();
+                _view.DataContext = _viewModel;
+
+                // 2. Tạo cầu nối ElementHost
+                ElementHost host = new ElementHost();
+                host.AutoSize = true;
+                host.Dock = System.Windows.Forms.DockStyle.Fill;
+                host.Child = _view; // Nhét WPF vào trong cầu nối
+
+                // 3. Thêm cầu nối vào PaletteSet (chứ không thêm trực tiếp _view)
+                _ps.Add("Attributes", host);
+
+                // -----------------------------
+            }
+            _ps.Visible = true;
+        }
     }
 }
